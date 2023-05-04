db076f02979fc02cd4e889754b08d08d6e1b4e6b
jamiulislam10 <jamiulislam156@gmail.com>
2023-05-04 19:10:50 +0600

    add cards details

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json');
+// const news = require('./data/news.json')
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Dragon is running')
+});
+
+app.get('/categories', (req, res) => {
+    console.log(categories);
+    res.send(categories);
+});
+
+// app.get('/news', (req, res) => {
+//     res.send(news);
+// })
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = categories?.find(n => n.id ==id);
+    res.send({selectedNews})
+    console.log(selectedNews)
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
+})
